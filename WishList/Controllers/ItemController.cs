55c2c62448f5502beb42d0eaa9619611896df569
Monika <m.szymanska.macik@gmail.com>
2020-12-17 11:17:21 +0100

    created itemcontrollers index action

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
@@ -18,7 +19,8 @@ namespace WishList.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model =_context.Items.ToList();
+            return View("Index", model);
         }
     }
 }
